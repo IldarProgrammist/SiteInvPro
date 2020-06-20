29fefe4d3d6d998a623b83713593433925413158
IldarProgrammist <programmerildarik@gmail.com>
2020-06-20 21:27:52 +0300

    исправление опечаток

--- InvPro/Catrige/models.py
+++ InvPro/Catrige/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+#Цвета
+class Color(models.Model):
+    name = models.CharField('Цвет', max_length=10)
+    imageColor = models.ImageField()
+
+    class Meta:
+        verbose_name = 'Цвет'
+        verbose_name_plural='Цвета'
+
+        def __str__(self):
+            return self.name
