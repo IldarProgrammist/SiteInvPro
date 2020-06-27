c915bf263a1f18cbd8d411753e32d7d2e989ee6f
IldarProgrammist <programmerildarik@gmail.com>
2020-06-27 13:39:43 +0300

    Добавление autocomplete для CatrigeAdmin

--- InvPro/Catrige/admin.py
+++ InvPro/Catrige/admin.py
@@ -1,23 +1,21 @@
 from django.contrib import admin
-from Catrige import models
 from Catrige.models import  CatrigeModel, Catrige, JurnalCatrige
 
 
 @admin.register(CatrigeModel)
 class CatrigeModelAdmin(admin.ModelAdmin):
     list_display = ['name', 'color', 'printerModel']
-    search_fields = ['color__name']
+    search_fields = ['name']
 
 
 @admin.register(Catrige)
 class CatrigeAdmin(admin.ModelAdmin):
     list_display = ['serialNumber', 'modelCatrige', 'original']
     search_fields = ['serialNumber']
+    autocomplete_fields = ('modelCatrige',)
 
 
 @admin.register(JurnalCatrige)
 class JurnalCatrigeAdmin(admin.ModelAdmin):
     list_display = ('serialNumberCatrige','status','dateExtation')
     search_fields = ['serialNumberCatrige__serialNumber']
-
-
